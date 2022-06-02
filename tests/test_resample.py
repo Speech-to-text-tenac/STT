633a315a37dc19d94e4d3255f18bae4e6c36f037
Samuel Alene <smlalene@gmail.com>
2022-06-02 10:47:15 -0700

    small change to test

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -13,7 +13,7 @@ class TestResampling(unittest.TestCase):
         self.df = TestResampling(data[:5])
 
     def test_check_resampling(self):
-        sampled_audio, rates = resample(data, 'key')
+        sampled_audio, rates = resample(df, 'key')
         expected = [44100, 44100, 44100, 44100, 44100]
         self.assertEqual(rates[8:13], expected)
 
