a2c92be0993405e3e845510820dbb41cbaa3e7c5
Yididya <yidisam18@gmail.com>
2022-06-02 15:06:21 +0300

    refactored scripts

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,7 +1,7 @@
 import math
 import random
-import warnings
 import sys
+import warnings
 
 import librosa  # for audio processing
 import matplotlib.pyplot as plt
@@ -9,9 +9,10 @@ import numpy as np
 import torch
 import torchaudio
 from IPython.display import Audio, display
+from logger import Logger
+from pydub import AudioSegment
 from scipy.io import wavfile  # for audio processing
 from torchaudio import transforms
-from logger import Logger
 
 warnings.filterwarnings("ignore")
 
@@ -31,78 +32,75 @@ class AudioUtil():
 
     def open(self, audio_file):
         """Load an audio file. Return the signal as a tensor and the sample rate."""
-         try:
-                self.logger.info(
-                    'Opening audio file for data preprocessing')
-                sig, sr = torchaudio.load(audio_file)
-                return (sig, sr)
-            except Exception as e:
-                self.logger.error('Failed to load data')
-                self.logger.error(e)
-                sys.exit(1)
+        try:
+            self.logger.info(
+                'Opening audio file for data preprocessing')
+            sig, sr = torchaudio.load(audio_file)
+            return (sig, sr)
+        except Exception as e:
+            self.logger.error('Failed to load data')
+            self.logger.error(e)
+            sys.exit(1)
 
-        def resize_audio(self, audios: dict,max_duration:float) -> dict:
-            """Extend duration of audio samples to max_duration.
+    def convert_to_stereo(self, audio_path, dest_path, new_channel) -> None:
+        """Convert the audio to stereo."""
+        try:
+            for file in audio_path:
+                sound = AudioSegment.from_file(file, format="wav")
+                if sound.channels == new_channel:
+                    continue
+                else:
+                    sound = sound.set_channels(new_channel)
 
-            Args:
-                    audios (dict): Dictionary of audio samples.
-                    max_duration (float): The duration to set for the audio samples
+                file_name = dest_path + file.split('/')[-1]
+                sound.export(file_name, format="wav")
+            self.logger.info("Successfully converted audio to stereo")
+        except Exception as e:
+            self.logger.error('Failed to convert to stereo')
+            self.logger.error(e)
+            sys.exit(1)
 
-            Returns:
-                    dict: Dictionary of resized audio samples.
-            """
-            try:
-                self.logger.info("Resizing audio samples")
-                resized_audios = {}
-                for label in audios:
-                    resized_audios[label] = librosa.util.fix_length(audios[label], size=int(max_duration*44100))
-                return resized_audios
-            except Exception as e:
-                self.logger.error('Failed to resize audio')
-                self.logger.error(e)
-                sys.exit(1)
+    def resize_audio(self, audios: dict, max_duration: float) -> dict:
+        """Extend duration of audio samples to max_duration.
+
+        Args:
+                audios (dict): Dictionary of audio samples.
+                max_duration (float): The duration to set for the audio samples
+
+        Returns:
+                dict: Dictionary of resized audio samples.
+        """
+        try:
+            self.logger.info("Resizing audio samples")
+            resized_audios = {}
+            for label in audios:
+                resized_audios[label] = librosa.util.fix_length(
+                    audios[label], size=int(max_duration*44100))
+            return resized_audios
+        except Exception as e:
+            self.logger.error('Failed to resize audio')
+            self.logger.error(e)
+            sys.exit(1)
         # ----------------------------
         # Show a widget to play the audio sound
         # ----------------------------
 
-        @staticmethod
-        def play(self, aud):
+    def play(self, aud):
+        """Play the audio signal."""
+        try:
+            self.logger.info('Playing the audio signal')
             sig, sr = aud
-            display(Audio(data=sig, rate=sr))
+            display(Audio(sig.numpy(), rate=sr))
+        except Exception as e:
+            self.logger.error(
+                'Failed to Play Audio Signal')
+            self.logger.error(e)
+            sys.exit(1)
 
-        @staticmethod
-        def rechannel(aud, new_channel):
-            sig, sr = aud
-            # resig = sig
-
-            if (sig.shape[0] == new_channel):
-                # Nothing to do
-                return aud
-
-            if (new_channel == 1):
-                # Convert from stereo to mono by selecting only the first channel
-                resig = sig[:1, :]
-            else:
-                # Convert from mono to stereo by duplicating the first channel
-                resig = torch.cat([sig, sig])
-
-            return ((resig, sr))
-
-        # or we can open and resample the data the same time
-        # resampling any loaded audio files to 44.1KHZ
-        @staticmethod
-        def resample(df, column):
-            sampled_audio = []
-            rates = []
-            for i in df[column]:
-                audio, rate = librosa.load(i, sr=44100)
-                sampled_audio.append(audio)
-                rates.append(rate)
-
-            return (sampled_audio, rates)
-
-        @staticmethod
-        def resample(aud, newsr):
+    def resample(self, aud, newsr):
+        """Resample the signal to a new sample rate."""
+        try:
+            self.logger.info('Resampling the signal')
             sig, sr = aud
 
             if (sr == newsr):
@@ -118,12 +116,19 @@ class AudioUtil():
                 resig = torch.cat([resig, retwo])
 
             return ((resig, newsr))
+        except Exception as e:
+            self.logger.error(
+                'Failed to resample audio')
+            self.logger.error(e)
+            sys.exit(1)
 
-        # ----------------------------
-        # Pad (or trim) the signal to a fixed length 'max_ms' in milliseconds
-        # ----------------------------
-        @staticmethod
-        def pad_trim(aud, max_ms):
+    # ----------------------------
+    # Pad (or trim) the signal to a fixed length 'max_ms' in milliseconds
+    # ----------------------------
+    def pad_trim(self, aud, max_ms):
+        """Trim or pad the signal to a fixed length 'max_ms' in milliseconds."""
+        try:
+            self.logger.info('Padding or trimming the signal')
             sig, sr = aud
             num_rows, sig_len = sig.shape
             max_len = sr//1000 * max_ms
@@ -144,61 +149,67 @@ class AudioUtil():
                 sig = torch.cat((pad_begin, sig, pad_end), 1)
 
             return (sig, sr)
+        except Exception as e:
+            self.logger.error(
+                'Failed to trim or pad audio')
+            self.logger.error(e)
+            sys.exit(1)
 
-        # ----------------------------
-        # Shifts the signal to the left or right by some percent. Values at the end
-        # are 'wrapped around' to the start of the transformed signal.
-        # ----------------------------
-        @staticmethod
-        def signal_shift(self,aud, max_shift_pct):
-            
-            try:
-                self.logger.info(
-                    'Shifts the signal to the left or right by some percent')
-                self.sig, sr = aud
-                roll_by = int(random.random()*max_shift_pct*len(sig[0]))
-                return (self.sig.roll(roll_by), sr)
-            except Exception as e:
-                self.logger.error('Failed to shift data')
-                self.logger.error(e)
-                sys.exit(1)
-        # ----------------------------
-        # Generate a Spectrogram
-        # ----------------------------
-        @staticmethod
-        def spectro_gram(self,aud, spectro_type='mel', n_mels=64, n_fft=1024, hop_len=None):
-            try:
-                self.sig, sr = aud
-                f_min, f_max, ws, top_db, pad = 0.0, None, None, 80, 0
+    # ----------------------------
+    # Shifts the signal to the left or right by some percent. Values at the end
+    # are 'wrapped around' to the start of the transformed signal.
+    # ----------------------------
+    def signal_shift(self, aud, max_shift_pct):
+        """Shift the signal to the left or right by some percent. Values at the end."""
+        try:
+            self.logger.info(
+                'Shifts the signal to the left or right by some percent')
+            self.sig, sr = aud
+            roll_by = int(random.random()*max_shift_pct*len(sig[0]))
+            return (self.sig.roll(roll_by), sr)
+        except Exception as e:
+            self.logger.error('Failed to shift data')
+            self.logger.error(e)
+            sys.exit(1)
+    # ----------------------------
+    # Generate a Spectrogram
+    # ----------------------------
 
-                # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
-                if (spectro_type == 'mel'):
-                    spec = transforms.MelSpectrogram(
-                        sr, n_fft, ws, hop_len, f_min, f_max, pad, n_mels)(sig)
-                elif (spectro_type == 'mfcc'):
-                    pass
-                else:
-                    spec = transforms.Spectrogram(
-                        n_fft, ws, hop_len, pad, normalize=False)(sig)
+    def spectro_gram(self, aud, spectro_type='mel', n_mels=64, n_fft=1024, hop_len=None):
+        """Generate a spectrogram from an audio file."""
+        try:
+            self.sig, sr = aud
+            f_min, f_max, ws, top_db, pad = 0.0, None, None, 80, 0
 
-                # Convert to decibels
-                spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
-                return (spec)
-            except Exception as e:
-                self.logger.error('Failed to convert decibels')
-                self.logger.error(e)
-                sys.exit(1)
+            # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+            if (spectro_type == 'mel'):
+                spec = transforms.MelSpectrogram(
+                    sr, n_fft, ws, hop_len, f_min, f_max, pad, n_mels)(sig)
+            elif (spectro_type == 'mfcc'):
+                pass
+            else:
+                spec = transforms.Spectrogram(
+                    n_fft, ws, hop_len, pad, normalize=False)(sig)
 
+            # Convert to decibels
+            spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
+            return (spec)
+        except Exception as e:
+            self.logger.error('Failed to convert decibels')
+            self.logger.error(e)
+            sys.exit(1)
 
-        # ----------------------------
-        # Augment the Spectrogram by masking out some sections of it in both the frequency
-        # dimension (ie. horizontal bars) and the time dimension (vertical bars) to prevent
-        # overfitting and to help the model generalise better. The masked sections are
-        # replaced with the mean value.
-        # ----------------------------
+    # ----------------------------
+    # Augment the Spectrogram by masking out some sections of it in both the frequency
+    # dimension (ie. horizontal bars) and the time dimension (vertical bars) to prevent
+    # overfitting and to help the model generalise better. The masked sections are
+    # replaced with the mean value.
+    # ----------------------------
 
-        @staticmethod
-        def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+    def spectro_augment(self, spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+        """Augment the Spectrogram by masking out some sections of it in both the frequency."""
+        try:
+            self.logger.info('Augmenting the Spectrogram')
             _, n_mels, n_steps = spec.shape
 
             # Frequency Masking: frequency channels [f0, f0 + f) are masked. f is chosen from a
@@ -226,21 +237,37 @@ class AudioUtil():
                 t0 = random.randint(0, n_steps-t)
                 spec[0][:, t0:t0+t] = fill
             return spec
+        except Exception as e:
+            self.logger.error(
+                'Failed to augment the Spectrogram')
+            self.logger.error(e)
+            sys.exit(1)
+    # ----------------------------
+    # Plot the audio signal
+    # ----------------------------
 
-        # ----------------------------
-        # Plot the audio signal
-        # ----------------------------
-        def show_wave(aud, label='', ax=None):
+    def show_wave(self, aud, label='', ax=None):
+        """Show the waveform of the audio signal."""
+        try:
+            self.logger.info('Plotting the audio signal')
             sig, sr = aud
             if (not ax):
                 _, ax = plt.subplots(1, 1, figsize=(3, 3))
             ax.plot(sig[0])
             ax.set_title(label)
+        except Exception as e:
+            self.logger.error(
+                'Failed to show waveform of audio signal')
+            self.logger.error(e)
+            sys.exit(1)
 
-        # ----------------------------
-        # Plot the audio signal before and after a transform
-        # ----------------------------
-        def show_transform(orig, trans):
+    # ----------------------------
+    # Plot the audio signal before and after a transform
+    # ----------------------------
+    def show_transform(self, orig, trans):
+        """Plot transform."""
+        try:
+            self.logger.info("Plotting transorm.")
             osig, osr = orig
             tsig, tsr = trans
             if orig is not None:
@@ -250,12 +277,27 @@ class AudioUtil():
             plt.legend()
             plt.show()
 
-        # ----------------------------
-        # Plot the spectrogram
-        # ----------------------------
-        def show_spectro(spec, label='', ax=None, figsize=(6, 6)):
+        except Exception as e:
+            self.logger.error(
+                'Failed to to transorm')
+            self.logger.error(e)
+            sys.exit(1)
+    # ----------------------------
+    # Plot the spectrogram
+    # ----------------------------
+
+    def show_spectro(self, spec, label='', ax=None, figsize=(6, 6)):
+        """Plot spectrogram."""
+        try:
+            self.logger.info('Plotting the spectrogram')
             if (not ax):
                 _, ax = plt.subplots(1, 1, figsize=figsize)
             # Reduce first dimension if it is greyscale
             ax.imshow(spec if (spec.shape[0] == 3) else spec.squeeze(0))
             ax.set_title(f'{label}, {list(spec.shape)}')
+
+        except Exception as e:
+            self.logger.error(
+                'Failed to show spectrogram')
+            self.logger.error(e)
+            sys.exit(1)
