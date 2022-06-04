639c0696bf80603a8b17d5f8580dd108add3a5b1
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-06-04 11:27:55 +0300

    changes on preprocessing

--- scripts/data_prepro.py
+++ scripts/data_prepro.py
@@ -29,17 +29,14 @@ class AudioUtil():
         #     self.logger.error(e)
         #     sys.exit(1)
 
-    def open(self, audio_file):
-        """Load an audio file. Return the signal as a tensor and the sample rate."""
-        # try:
-        #     # self.logger.info(
-        #     #     'Opening audio file for data preprocessing')
-        #     sig, sr = torchaudio.load(audio_file)
-        #     return (sig, sr)
-        # except Exception as e:
-        #     # self.logger.error('Failed to load data')
-        #     # self.logger.error(e)
-        #     sys.exit(1)
+    def create_meta_data( self, df: pd.DataFrame, column1:str, column2:str):
+        df.rename(columns = {0: column1}, inplace = True)
+        df[column2] = df[column1].apply(lambda x: x.split("</s>")[1].replace("(", "").replace(")", "").strip())
+        df[column1] = df[column1].apply(lambda x: x.split("</s>")[0])
+        df[column1] = df[column1].apply(lambda x: x.split("<s>")[1].strip())
+        df[column2] = df[column2].apply(lambda x: "data/train/wav/"+x+".wav")
+        return df
+
 
     def convert_to_stereo(self, audio_path, dest_path, new_channel) -> None:
         """Convert the audio to stereo."""
@@ -59,27 +56,44 @@ class AudioUtil():
             # self.logger.error(e)
             sys.exit(1)
 
-    def resize_audio(self, audios: dict, max_duration: float) -> dict:
-        """Extend duration of audio samples to max_duration.
+  
+    def resample(self, df, column):
+        sampled_audio = []
+        rates = []
+        for i in df[column]:
+            audio, rate=librosa.load(i, sr=44100)
+            sampled_audio.append(audio)
+            rates.append(rate)
+        
+        return sampled_audio, rates
+    
+    def resize_audio(self,audios,max_duration):
+        resized_audios = {}
+        for label in audios:
+            resized_audios[label] = librosa.util.fix_length(audios[label],size=int(max_duration*44100))
+        return resized_audios
 
-        Args:
-                audios (dict): Dictionary of audio samples.
-                max_duration (float): The duration to set for the audio samples
+    # def resize_audio(self, audios: dict, max_duration: float) -> dict:
+    #     """Extend duration of audio samples to max_duration.
 
-        Returns:
-                dict: Dictionary of resized audio samples.
-        """
-        try:
-            # self.logger.info("Resizing audio samples")
-            resized_audios = {}
-            for label in audios:
-                resized_audios[label] = librosa.util.fix_length(
-                    audios[label], size=int(max_duration*44100))
-            return resized_audios
-        except Exception as e:
-            # self.logger.error('Failed to resize audio')
-            # self.logger.error(e)
-            sys.exit(1)
+    #     Args:
+    #             audios (dict): Dictionary of audio samples.
+    #             max_duration (float): The duration to set for the audio samples
+
+    #     Returns:
+    #             dict: Dictionary of resized audio samples.
+    #     """
+    #     try:
+    #         # self.logger.info("Resizing audio samples")
+    #         resized_audios = {}
+    #         for label in audios:
+    #             resized_audios[label] = librosa.util.fix_length(
+    #                 audios[label], size=int(max_duration*44100))
+    #         return resized_audios
+    #     except Exception as e:
+    #         # self.logger.error('Failed to resize audio')
+    #         # self.logger.error(e)
+    #         sys.exit(1)
         # ----------------------------
         # Show a widget to play the audio sound
         # ----------------------------
