ab4389775e85155e50b5c760ec6231f32918453b
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-06-05 23:34:01 +0300

    uploading audio

--- stt-web-app/src/pages/Prediction.js
+++ stt-web-app/src/pages/Prediction.js
@@ -1,19 +1,109 @@
-// import classes from "./Prediction.module.css";className={classes.display}
+import classesp from "./Prediction.module.css";
+import axios from "axios";
+
+let gumStream = null;
+let recorder = null;
+let audioContext = null;
 
 export const Prediction = () => {
+  // return (
+  //   <>
+  //     {/* <div> */}
+  //     <div>
+  //       <h2>Prediction</h2>
+  //       <input type="file"></input>
+  //       <button>predict</button>
+  //     </div>
+
+  //     <div>
+  //       <h2>output</h2>
+  //     </div>
+  //     {/* </div> */}
+  //   </>
+  // );
+  const startRecording = () => {
+    let constraints = {
+      audio: true,
+      video: false,
+    };
+  
+    audioContext = new window.AudioContext();
+    console.log("sample rate: " + audioContext.sampleRate);
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (stream) {
+        console.log("initializing Recorder.js ...");
+
+        gumStream = stream;
+
+        let input = audioContext.createMediaStreamSource(stream);
+        console.log(input)
+        
+        recorder = new window.Recorder(input, {
+          numChannels: 1,
+        });
+        
+        console.log(recorder);
+        recorder.record();
+        console.log("Recording started");
+      })
+      .catch(function (err) {
+        //enable the record button if getUserMedia() fails
+        console.log(err)
+      });
+  };
+
+  const stopRecording = () => {
+    console.log("stopButton clicked");
+
+    recorder.stop(); //stop microphone access
+    gumStream.getAudioTracks()[0].stop();
+
+    recorder.exportWAV(onStop);
+  };
+
+  const onStop = (blob) => {
+    console.log("uploading...");
+
+    let data = new FormData();
+
+    data.append("text", "this is the transcription of the audio file");
+    data.append("wavfile", blob, "recording.wav");
+
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+    console.log(data)
+    axios.post("http://localhost:8080/asr/", data, config);
+  };
+
   return (
     <>
-      {/* <div> */}
-      <div>
-        <h2>Prediction</h2>
-        <input type="file"></input>
-        <button>predict</button>
-      </div>
+      {/* <div className={classesp.display}>
+        <button onClick={startRecording} type="button">
+          Start
+        </button>
+        <button onClick={stopRecording} type="button">
+          Stop
+        </button>
+        <br></br>
+        <br></br>
+        <audio autoplay controls></audio>
 
-      <div>
-        <h2>output</h2>
+        <section>
+          <p>HELLO, EVERYONE</p>
+        </section>
+      </div> */}
+
+      <div className={classesp.upload}>
+        <h2>Upload Audio</h2>
+        <section>
+          <input type="file" />
+          <button>Predict</button>
+        </section>
       </div>
-      {/* </div> */}
     </>
   );
 };
+
--- stt-web-app/src/pages/Spectogram.js
+++ stt-web-app/src/pages/Spectogram.js
@@ -1,6 +1,26 @@
+import { ReactMediaRecorder, useReactMediaRecorder } from "react-media-recorder"
+import classesp from "./Prediction.module.css";
+//import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+
 export const Spectogram = () => {
-  return(
-  <div>Spectogram</div>
-  ) 
+  // const {status, startRecording, stopRecording, mediaBlobUrl} = useReactMediaRecorder({video: true})
+  return (
+    <ReactMediaRecorder
+      audio
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+        <div className={classesp.display}>
+          <p>{status}</p>
+          <button onClick={startRecording}>Start</button>
+          <button onClick={stopRecording}>Stop</button>
+          <br></br>
+          <br></br>
+          <audio src={mediaBlobUrl} autoPlay loop controls></audio>
+          <section>
+            <p>HELLO, EVERYONE</p>
+          </section>
+        </div>
+      )}
+    />
+  ); 
 }
 
