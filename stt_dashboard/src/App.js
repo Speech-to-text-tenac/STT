c307f6599dec8009a7c7de2831908b36b47d2d42
nardoshood <nardostilahun83@gmail.com>
2022-06-07 15:05:14 +0300

    Route the recorder page

--- stt_dashboard/src/App.js
+++ stt_dashboard/src/App.js
@@ -1,26 +1,14 @@
-// import './App.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from "react-bootstrap/Nav";
+import * as React from "react";
+import Records from "./elements/pages/record";
+// import Home from "./view/pages/home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          
-        <Navbar bg="dark" variant="dark">
-          
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Visuals</Nav.Link>
-            <Nav.Link href="#pricing">Upload your audio</Nav.Link>
-          </Nav>
-          
-        </Navbar>
-  
-
-      </header>
-    </div>
+	  
+    <div>
+		<header></header>
+			<Records />
+		</div>
   );
 }
 
