dd3a6da7de7744e893badecf45783ed6954d6053
Yididya <yidisam18@gmail.com>
2022-06-08 10:58:44 +0300

    prediction try app

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 # coding=utf-8
-import sys
 import os
-import shutil
-import glob
-import re
 import numpy as np
 import pandas as pd
 
@@ -11,26 +7,35 @@ import pandas as pd
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './scripts')))
+from prediction import Prediction
+
+# Instantiate prediction class
+prediction = Prediction()
 
 # Define a flask app
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
+# Upload folder for storing files
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def home_view():
-    return "<h1>Welcome to STT engine for Amharic language</h1>"
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return {
+        "status": "sucess",
+        "message": "Amharic Speech Recognition API",
+    }
 
-# @app.route('/', methods=['GET'])
-# def index():
-#     # Main page
-#     return {
-#         "status": "sucess",
-#         "message": "Hello World"
-#     }
-
+@app.route('/predict', methods=['GET', 'POST'])
+def handle_upload():
+    if request.method == 'POST':
+        return prediction.handle_df_upload(request, secure_filename, app)
+    elif request.method == 'GET':
+        return {"status": "fail", "error": "No Get Route Supported in /predict endpoint"}
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 33507))
